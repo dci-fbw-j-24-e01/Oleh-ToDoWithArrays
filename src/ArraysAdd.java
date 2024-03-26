e14c86d966152beb461900c878e71b82a858767b
Herr Oleh Bryl <olegbryl@gmail.com>
2024-03-26 09:27:51 +0100

    change3

--- src/ArraysAdd.java
+++ src/ArraysAdd.java
@@ -3,7 +3,7 @@ public class ArraysAdd {
     static int[] numbers2 = {2, 5, 10, 6, 3, 12};
     int[] numbers3 = {2, 5, 10, 6, 3, 12, 20, 4, 15, 10};
 
-    int[] numbers4 = {4, 7, 10, 12, 1, 8, 20, 12};
+    int[] numbers4 = {4, 7, 10, 12, 1, 8, 5, 20, 12};
 
     String[] names = {"Josie", "Cathrin", "Bob", "Max", "Hannah", "Alex"};
 
--- src/CheckingNumbers.java
+++ src/CheckingNumbers.java
@@ -1,22 +1,49 @@
-public class CheckingNumbers extends ArraysAdd{
+public class CheckingNumbers extends ArraysAdd {
+    static int n = 5;
+    boolean found = false;
 
+    public CheckingNumbers() {
 
-    public  CheckingNumbers(){
-        int n = 5;
+        if (checkNumber(numbers4, n)) {
+            System.out.println("Number 5 exists inside an array");
+        }else {
+            System.out.println("No. I couldn't find it.");
+        }
+
+//        int n = 5;
+//        boolean found = false;
+//        System.out.println("Now I'm going to look for the number 5 in the array.");
+//        for (int i = 0; i < numbers4.length; i++) {
+//            if (n == numbers4[i]){
+//                System.out.println("Number 5 exists inside an array");
+//            } else {
+//                System.out.println("No. I couldn't find it.");
+//            }
+//            if (n == numbers4[i]){
+//                found = true;
+//            }
+
+//            for (int check : numbers4) {
+//                if (check == n) {
+//                    found = true;
+//                    break;
+//                }
+//                if (found) System.out.println("Number 5 exists inside an array");
+//                else System.out.println("No. I couldn't find it.");
+//
+//            }
+//        }
+
+//        System.out.println("Number 5 exists inside an array" + found);
+    }
+    public static boolean checkNumber(int[] numbers4, int n){
         boolean found = false;
-        System.out.println("Now I'm going to look for the number 5 in the array.");
         for (int i = 0; i < numbers4.length; i++) {
 
-
-        for (int check: numbers4){
-            if (check == n){
-                found = true;
-                break;
+            if (n == numbers4[i]){
+                return true;
             }
-            if (found) System.out.println("Number 5 exists inside an array");
-            else System.out.println("No. I couldn't find it.");
-
         }
-        }
+        return false;
     }
 }
